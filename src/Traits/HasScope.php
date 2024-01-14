739802079d2bef274eb38885f3e0afbfb38c1040
martanto <martanto@users.noreply.github.com>
2024-01-14 06:09:50 +0000

    Fix styling

--- src/Traits/HasScope.php
+++ src/Traits/HasScope.php
@@ -8,10 +8,6 @@ trait HasScope
 {
     /**
      * Get model by scope
-     *
-     * @param Builder $query
-     * @param string $code
-     * @return void
      */
     public function scopeByCode(Builder $query, string $code): void
     {
