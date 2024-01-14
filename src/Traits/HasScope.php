fc4a63c524aff86e3137241d9396f95bb669e82c
Martanto <martanto@live.com>
2024-01-14 13:09:28 +0700

    Update HasScope.php

--- src/Traits/HasScope.php
+++ src/Traits/HasScope.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait HasScope
 {
+    /**
+     * Get model by scope
+     *
+     * @param Builder $query
+     * @param string $code
+     * @return void
+     */
     public function scopeByCode(Builder $query, string $code): void
     {
         $query->where('code', $code);
